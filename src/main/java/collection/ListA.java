31bb122d5be76db94bee9f4bdb467096e3380996
DahunJeongINU <gnsgnsek@gmail.com>
2019-11-14 16:27:51 +0900

    [pandahun] ListA if문 안의 메소드 추출 #4
    #4 if문 안의 메소드 추출
    if문 내의 연산 로직을 메소드로 추출해 가독성 높였습니다.

--- src/main/java/collection/ListA.java
+++ src/main/java/collection/ListA.java
@@ -1,15 +1,17 @@
 package collection;
 
 public class ListA {
+	public static final int STORE_SIZE_INCREASEMENT 10;
+	
 	public Object[] elements = new Object[10];
 	public boolean readOnly;
 	public int size = 0;
-
+	
 	public void add(Object element) {
 		if (!readOnly) {
 			int newSize = size + 1;
 			
-			if ( newSize > elements.length) {
+			if (isElementStoreFull(newSize)) {
 				Object[] newElements = new Object[elements.length + 10];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
@@ -21,6 +23,10 @@ public class ListA {
 			elements[size] = element;
 			size++;
 		}
+	}
+	
+	public static boolean isElementStoreFull(int newSize){
+		return newSize > elements.lengths;
 	}
 	
 	public boolean setReadOnly(boolean ro) {
